265d337e483526c78378c1bb5e6b12bebaa2ad55
Axel Stankiewicz <axelxbox126@gmail.com>
2022-01-09 13:05:38 +0100

    Phase 2 : premier test "Hello Worlds!"

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AcceuilController
+ *
+ * @author axelx
+ */
+
+class AcceuilController {
+    
+    /**
+     * @Route("/", name="acceuil")
+     * @return Response
+     */
+    public function index(): Response{
+        return new Response('Hello world!');
+    }
+}
