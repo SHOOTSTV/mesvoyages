82dd130ff914e3db497fbb8da3416c5d214158bc
Axel Stankiewicz <axelxbox126@gmail.com>
2022-01-12 21:58:55 +0100

    Phase 13 : ORM (modification d'un enregistrement, création d'un formulaire)

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -59,4 +59,25 @@ class AdminVoyagesController extends AbstractController {
         $this->om->flush();
         return $this->redirectToRoute('admin.voyages');
     }  
+    
+    /**
+     * @Route("/admin/edit/{id}", name="admin.voyage.edit")
+     * @param Visite $visite
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Visite $visite, Request $request): Response{
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->om->flush();
+            return $this->redirectToRoute('admin.voyages');
+        }
+        
+        return $this->render("admin/admin.voyage.edit.html.twig", [
+            'visite' => $visite,
+            'formvisite' => $formVisite->createView()
+        ]);
+    }
 }
